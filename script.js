fbc4ab403665192004f361b515e427e733c8f7e3
michael <sipb64@gmail.com>
2025-01-16 14:14:10 +0100

    site v2

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Merci pour votre message! Nous vous recontacterons très prochainement.');
+});
