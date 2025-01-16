d275cc0312d368ca1d247eaf26a25ba6f69991b2
michael <sipb64@gmail.com>
2025-01-16 14:13:30 +0100

    site V2 -delete v1

--- V1/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const header = document.querySelector('header');
-    header.addEventListener('click', function() {
-        alert('Bienvenue sur mon site !');
-    });
-});
