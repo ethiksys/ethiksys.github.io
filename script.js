898c6274b7e393a728883c46bb8947921b5117f1
sipb64 <sipb64@gmail.com>
2025-02-21 21:22:50 +0100

    v0.3

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Merci pour votre message! Nous vous recontacterons très prochainement.');
-});
