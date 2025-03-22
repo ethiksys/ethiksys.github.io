a8ceab9941826cf86d05974fae6bf173cb6b68ae
sipb64 <sipb64@gmail.com>
2025-03-22 17:14:03 +0100

    V220325

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    
+
     // Validation du formulaire
     if (thisElementsAreFilled()) {
-        showConfirmationMessage('Le formulaire semble complété. Merci !');
+      showConfirmationMessage("Le formulaire semble complété. Merci !");
     } else {
-        alert('Veuillez remplir tous les champs obligatoires.');
+      alert("Veuillez remplir tous les champs obligatoires.");
     }
-    
+
     // Supprimer le formulaire après validation
     event.target.reset();
-});
+  });
 
 function thisElementsAreFilled() {
-    const requiredFields = ['nom', 'prenom', 'email'];
-    if (!requiredFields.every(field => document.getElementById(field).value)) {
-        return false;
-    }
-    return true;
+  const requiredFields = ["name", "email", "message"];
+  if (!requiredFields.every((field) => document.getElementById(field).value)) {
+    return false;
+  }
+  return true;
 }
 
 function showConfirmationMessage(message) {
-    alert(`${message}\nVeuillez attendre un message de retour de l'=?,=e serveur...`);
+  alert(
+    `${message}\nVeuillez attendre un message de retour de l'=?,=e serveur...`
+  );
 }
